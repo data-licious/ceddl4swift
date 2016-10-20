664df1a86c4ce7dd6d2d0c00d6312208a3788a98
Sachin Vas <svas@datalicious.com>
2016-10-20 11:05:23 +0530

    [ST-27651] - initial commit

---
+++ ceddl4swift/ceddl4swift/ceddl4swift.h
@@ -0,0 +1,19 @@
+//
+//  ceddl4swift.h
+//  ceddl4swift
+//
+//  Created by Sachin Vas on 20/10/16.
+//  Copyright © 2016 Sachin Vas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ceddl4swift.
+FOUNDATION_EXPORT double ceddl4swiftVersionNumber;
+
+//! Project version string for ceddl4swift.
+FOUNDATION_EXPORT const unsigned char ceddl4swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ceddl4swift/PublicHeader.h>
+
+
